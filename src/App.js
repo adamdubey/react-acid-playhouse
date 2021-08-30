21c299dfc1a2ab5eb91c8d562cfd8a199f64eb1b
Adam Dubey <adubey0311@gmail.com>
2021-08-30 14:44:53 -0400

    [AD][08M3021] Provisioning Redux and adding dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import './App.css';
+import store from './redux/store';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Acid Playhouse</h1>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>Acid Playhouse</h1>
+      </div>
+    </Provider>
   );
 };
 
