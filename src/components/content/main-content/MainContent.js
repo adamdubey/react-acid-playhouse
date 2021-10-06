d3d3bfa2d21dea360d858774413b2280571d0c6f
Adam Dubey <adubey0311@gmail.com>
2021-10-06 14:06:10 -0400

    [AD][10M0621] Adding img fetch for slideshow and create loading spinner

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Paginate from '../paginate/Paginate';
 import Slideshow from '../slideshow/Slideshow';
 import Grid from '../grid/Grid';
 import './MainContent.scss';
+import { IMAGE_URL } from '../../../services/movies.service';
 
-const MainContent = () => {
-  const images = [
+const MainContent = (props) => {
+  const { list } = props;
+  const imagesArray = [
     {
       url: 'https://images.pexels.com/photos/1499344/pexels-photo-1499344.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
       rating: 4
@@ -21,6 +25,29 @@ const MainContent = () => {
   ];
 
   const [currentPage, setCurrentPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const randomMovies = list.sort(() => Math.random() - Math.random()).slice(0, 4);
+
+  useEffect(() => {
+    if (randomMovies.length) {
+      const IMAGES = [
+        {
+          id: 1,
+          url: `${IMAGE_URL}/${randomMovies[0].backdrop_path}`
+        },
+        {
+          id: 2,
+          url: `${IMAGE_URL}/${randomMovies[1].backdrop_path}`
+        },
+        {
+          id: 3,
+          url: `${IMAGE_URL}/${randomMovies[2].backdrop_path}`
+        }
+      ];
+      setImages(IMAGES);
+    }
+    // eslint-disable-next-line
+  }, []);
 
   const paginate = (type) => {
     if (type === 'prev' && currentPage >= 1) {
@@ -39,9 +66,17 @@ const MainContent = () => {
           <Paginate currentPage={currentPage} totalPages={10} paginate={paginate} />
         </div>
       </div>
-      <Grid images={images} />
+      <Grid images={imagesArrray} />
     </div>
   );
 };
 
-export default MainContent;
+MainContent.propTypes = {
+  list: PropTypes.array.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  list: state.movies.list
+});
+
+export default connect(mapStateToProps, {})(MainContent);
