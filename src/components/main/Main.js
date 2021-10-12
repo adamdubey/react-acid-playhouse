9579ae32f0c733aa760c62c83d5e4a3905ad40d1
Adam Dubey <adubey0311@gmail.com>
2021-10-12 12:27:34 -0400

    [AD][10M1221] Implement fetch data onscroll method

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,9 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import './Main.scss';
 import MainContent from '../content/main-content/MainContent';
 import Spinner from '../spinner/Spinner';
+import { loadMoreMovies, setResponsePageNumber } from '../../redux/actions/movies';
 
-const Main = () => {
+const Main = (props) => {
+  const { loadMoreMovies, page, totalPages, setResponsePageNumber } = props;
   const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(page);
+  const mainRef = useRef();
+  const bottomLineRef = useRef();
 
   useEffect(() => {
     setLoading(true);
@@ -12,7 +20,45 @@ const Main = () => {
     }, 3000);
   }, []);
 
-  return <div className="main">{loading ? <Spinner /> : <MainContent />}</div>;
+  useEffect(() => {
+    setResponsePageNumber(currentPage, totalPages);
+    loadMoreMovies('now_playing', currentPage);
+  }, [currentPage]);
+
+  const fetchData = () => {
+    if (page < totalPages) {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
+
+  const handleScroll = () => {
+    const containerHeight = mainRef.current.getBoundingClientRect().height;
+    const { top: bottomLineTop } = bottomLineRef.current.getBoundingClientRect();
+    if (bottomLineTop <= containerHeight) {
+      fetchData();
+    }
+  };
+
+  return (
+    <div className="main" ref={mainRef} onScroll={() => handleScroll()}>
+      {loading ? <Spinner /> : <MainContent />}
+      <div ref={bottomLineRef}></div>
+    </div>
+  );
 };
 
-export default Main;
+Main.propTypes = {
+  list: PropTypes.array,
+  page: PropTypes.number,
+  totalPages: PropTypes.number,
+  loadMoreMovies: PropTypes.func,
+  setResponsePageNumber: PropTypes.func
+};
+
+const mapStateToProps = (state) => ({
+  list: state.movies.list,
+  page: state.movies.page,
+  totalPages: state.movies.totalPages
+});
+
+export default connect(mapStateToProps, { loadMoreMovies, setResponsePageNumber })(Main);
--- src/redux/actions/movies.js
+++ src/redux/actions/movies.js
@@ -1,14 +1,10 @@
-import { MOVIE_LIST, SET_ERROR, RESPONSE_PAGE } from '../types';
+import { MOVIE_LIST, SET_ERROR, RESPONSE_PAGE, LOAD_MORE_RESULTS } from '../types';
 import { MOVIE_API_URL } from '../../services/movies.service';
 
 export const getMovies = (type, pageNumber) => async (dispatch) => {
   try {
-    const movies = await MOVIE_API_URL(type, pageNumber);
-    const { results, page, total_pages } = movies.data;
-    const payload = {
-      page,
-      totalPages: total_pages
-    };
+    const response = await getMoviesRequest(type, pageNumber);
+    const { results, payload } = response;
     dispatchMethod(MOVIE_LIST, results, dispatch);
     dispatchMethod(RESPONSE_PAGE, payload, dispatch);
   } catch (error) {
@@ -18,6 +14,34 @@ export const getMovies = (type, pageNumber) => async (dispatch) => {
   }
 };
 
+export const loadMoreMovies = (type, pageNumber) => async (dispatch) => {
+  try {
+    const response = await getMoviesRequest(type, pageNumber);
+    const { results, payload } = response;
+    dispatchMethod(LOAD_MORE_RESULTS, results, dispatch);
+    dispatchMethod(RESPONSE_PAGE, payload, dispatch);
+  } catch (error) {
+    if (error.response) {
+      dispatchMethod(SET_ERROR, error.response.data.message, dispatch);
+    }
+  }
+};
+
+export const setResponsePageNumber = (page, totalPages) => async (dispatch) => {
+  const payload = { page, totalPages };
+  dispatchMethod(RESPONSE_PAGE, payload, dispatch);
+};
+
 const dispatchMethod = (type, payload, dispatch) => {
   dispatch({ type, payload });
+};
+
+const getMoviesRequest = async (type, pageNumber) => {
+  const movies = await MOVIE_API_URL(type, pageNumber);
+  const { results, page, total_pages } = movies.data;
+  const payload = {
+    page,
+    totalPages: total_pages
+  };
+  return { results, payload };
 };
