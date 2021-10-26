9354b88b7a47606d93adb5234c1bdfabb4fcf4a9
Adam Dubey <adubey0311@gmail.com>
2021-10-26 08:39:40 -0400

    [AD][10M2621] Update lazy loading component

--- src/components/lazyImage/LazyImage.js
+++ src/components/lazyImage/LazyImage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-import placeHolder from '../../assets/logo.svg';
+import placeHolder from '../../assets/loading.gif';
 
 const LazyImage = (props) => {
   const { src, alt, children, className } = props;
